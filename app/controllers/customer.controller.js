7eaf03f57c3a9658d3f20bb7ecd0530217c657f8
Nutcras <nazreebang@gmail.com>
2022-07-03 22:03:53 +0700

    ver03-07-65

--- app/controllers/customer.controller.js
+++ app/controllers/customer.controller.js
@@ -247,7 +247,7 @@ exports.login = async (req, res) =>{
         message: err.message || 'Some error occurred.',
       })
     else if (data[0] && verifyingHash(password,data[0].cust_password)){
-        data[0].token = await sign({id: data[0].id},'3h')
+        data[0].token = await sign({id: data[0].cust_id},'3h')
         delete data[0].cust_password
         res.status(200).json(data[0])
     }
--- app/controllers/manager.controller.js
+++ app/controllers/manager.controller.js
@@ -139,9 +139,9 @@ exports.login = async (req, res) =>{
       res.status(err.status).send({
         message: err.message || 'Some error occurred.',
       })
-    else if (data[0] && verifyingHash(password,data[0].password)){
-        data[0].token = await sign({id: data[0].id},'3h')
-        delete data[0].password
+    else if (data[0] && verifyingHash(password,data[0].manager_password)){
+        data[0].token = await sign({id: data[0].manager_id},'3h')
+        delete data[0].manager_password
         res.status(200).json(data[0])
     }
     else res.status(204).end()

1d68f6122521e0dc5ac122d09d09203eac82fa81
Nutcras <nazreebang@gmail.com>
2022-07-20 11:19:41 +0700

    ver20-07-65

---
+++ app/controllers/review.controller.js
@@ -0,0 +1,104 @@
+const validate_req = require('../models/validate_req.models')
+const mysql = require('../models/mysql.models')
+
+exports.create = async (req, res) => {
+  //ดึงข้อมูลจาก request
+  const {score, review, cust_id, men_id} = req.body
+  //ตรวจสอบความถูกต้อง request
+  if (validate_req(req, res, [men_id,cust_id])) return
+  //คำสั่ง SQL
+  let sql = `INSERT INTO review SET ?`
+  //ข้อมูลที่จะใส่ ชื่อฟิล : ข้อมูล
+  let data = {
+    rev_id:id,
+    rev_score:score,
+    rev_review:review,
+    cust_id:cust_id,
+    men_id:men_id
+  }
+  //เพิ่มข้อมูล โดยส่งคำสั่ง SQL เข้าไป
+  await mysql.create(sql, data, async(err, data) => {
+
+    if (err)
+      res.status(500).send({
+        message: err.message || 'Some error occurred.',
+      })
+    else{
+      res.status(201).json(data)
+    }
+  })
+}
+
+exports.findAll = async (req, res) => {
+  //คำสั่ง SQL
+  let sql = `SELECT * FROM review`
+  //ดึงข้อมูล โดยส่งคำสั่ง SQL เข้าไป
+  await mysql.get(sql, (err, data) => {
+    if (err)
+      res.status(err.status).send({
+        message: err.message || 'Some error occurred.',
+      })
+    else if (data) { 
+      res.status(200).json(data) }
+    else res.status(204).end()
+  })
+}
+
+exports.findOne = async (req, res) => {
+  //ดึงข้อมูลจาก params
+  const { id } = req.params
+  // ตรวจสอบความถูกต้อง request
+  if (validate_req(req, res, [id])) return
+  //คำสั่ง SQL
+  let sql = `SELECT * FROM review WHERE men_id = ${id}`
+  //ดึงข้อมูล โดยส่งคำสั่ง SQL เข้าไป
+  await mysql.get(sql, (err, data) => {
+    if (err)
+      res.status(err.status).send({
+        message: err.message || 'Some error occurred.',
+      })
+    else if (data[0]) res.status(200).json(data[0])
+    else res.status(204).end()
+  })
+}
+
+exports.update = async (req, res) => {
+  //ดึงข้อมูลจาก request
+  const {  score, review } = req.body
+  //ดึงข้อมูลจาก params
+  const { id } = req.params
+  //ตรวจสอบความถูกต้อง request
+  if (validate_req(req, res, [id])) return
+  //คำสั่ง SQL
+  let sql = `UPDATE review SET rev_score =?, rev_review = ? WHERE rev_id = ?`
+  //ข้อมูลที่จะแก้ไขโดยเรียงตามลำดับ เครื่องหมาย ?
+  let data = [score, review, id]
+  //แก้ไขข้อมูล โดยส่งคำสั่ง SQL เข้าไป
+  await mysql.update(sql, data, (err, data) => {
+    if (err)
+      res.status(err.status).send({
+        message: err.message || 'Some error occurred.',
+      })
+    else res.status(204).end()
+  })
+}
+
+exports.deleteOne = async (req, res) => {
+  //ดึงข้อมูลจาก params
+  const { id } = req.params
+  //ตรวจสอบความถูกต้อง request
+  if (validate_req(req, res, [id])) return
+  //คำสั่ง SQL
+  let sql = `DELETE FROM review WHERE rev_id = ?`
+  //ข้อมูลที่จะแก้ไขโดยเรียงตามลำดับ เครื่องหมาย ?
+  let data = [id]
+  //ลบข้อมูล โดยส่งคำสั่ง SQL และ id เข้าไป
+  await mysql.delete(sql, data, (err, data) => {
+    if (err)
+      res.status(err.status).send({
+        message: err.message || 'Some error occurred.',
+      })
+    else res.status(204).end()
+  })
+}
+
---
+++ app/routes/review.route.js
@@ -0,0 +1,18 @@
+module.exports = (app) => {
+  const router = require('express').Router()
+  // const { verify } = require('../models/middleware.models.js')
+  const { create, findAll, findOne, update,  deleteOne} = require('../controllers/review.controller')
+
+  router.post('/', create)
+
+  router.get('/', findAll)
+
+  router.get('/:id', findOne)
+
+  router.put('/:id', update)
+  
+  router.delete('/:id', deleteOne)
+
+  //เซ็ต PREFIX
+  app.use(process.env.PREFIX + '/review', router)
+}

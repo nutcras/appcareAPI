94f8d404765c0406ba8b7569126656814dbe94fd
nutcras <83870987+nutcras@users.noreply.github.com>
2022-10-21 10:46:34 +0700

    อัพรูปตอนสร้างบัญชีและจอง

--- app/models/supabase.js
+++ app/models/supabase.js
@@ -1,14 +1,21 @@
 const  {createClient}  = require('@supabase/supabase-js')
 
-const supabase = createClient('https://lvspvwkgiozgxaoaurky.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx2c3B2d2tnaW96Z3hhb2F1cmt5Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY2Mzc2MTUyNywiZXhwIjoxOTc5MzM3NTI3fQ.Yd8am1BQUNIHOe9F3Uaxzsl0CUqPZLcOuaCP1ZsJxOk')
-const uploadImage = async (image) =>{
-  
+const supabase = createClient('https://qtzmtysieievsvunzhbe.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF0em10eXNpZWlldnN2dW56aGJlIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjYzMjE1NjUsImV4cCI6MTk4MTg5NzU2NX0.ZeNGdjE38KRjRF_FNNMWaRZ0LS1yNdnbJR8rHlYSXTE')
+exports.uploadImageAvatar = async (image) =>{
   const { data, error }= await supabase
         .storage
         .from('avatar')
         .upload(Math.random().toString(), image.buffer, { contentType: image.mimetype})
+  console.log(error);
+  return "https://qtzmtysieievsvunzhbe.supabase.co/storage/v1/object/public/"+data.Key
+}
+
+exports.uploadImageBook = async (image) =>{
+  const { data, error }= await supabase
+        .storage
+        .from('booking')
+        .upload(Math.random().toString(), image.buffer, { contentType: image.mimetype})
   
   console.log(error);  
-  return "https://lvspvwkgiozgxaoaurky.supabase.co/storage/v1/object/public/"+data.Key
+  return "https://qtzmtysieievsvunzhbe.supabase.co/storage/v1/object/public/"+data.Key
 }
-module.exports = uploadImage

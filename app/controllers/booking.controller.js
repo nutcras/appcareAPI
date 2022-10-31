8c5c5677de534ebd34020b4b18aaba42c6e2b7e3
nutcras <83870987+nutcras@users.noreply.github.com>
2022-10-31 18:19:53 +0700

    updateList

--- app/controllers/booking.controller.js
+++ app/controllers/booking.controller.js
@@ -106,7 +106,7 @@ exports.findDateBeforeCreate = async (req, res) => {
       res.status(err.status).send({
         message: err.message || 'Some error occurred.',
       })
-    else if (data) res.status(200).json(data[0])
+    else if (data) res.status(200).json(data)
     else res.status(204).end()
   })
 }
